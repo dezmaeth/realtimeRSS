943b2e589ecfd8c0973f82e8e4b528889c875b5e
Francisco Henseleit <dezmaeth@gmail.com>
2012-07-08 22:03:41 -0400

    functional

--- js/script.js
+++ js/script.js
@@ -2,15 +2,49 @@
 
 */
 
-$.ajax({ 
-	url: 'server/',
-	type: 'POST',
-	data: { getRSS : true },
-	success: function(data) {
-		
+'use strict';
 
-	},
-	error: function (jqXHR, textStatus, errorThrown) {
+var RSSreader = RSSreader = function () {
+	var _self = this,dom = {};
+
+	var _parse = function(data) {
+		var data = JSON.parse(data);
+		_drawFeed(data);
+	}
+
+	var _drawFeed = function(feed) {
+      	var news = ['<div class="row">',
+      		'<div class="span12 well">',
+      		feed.title,
+      		'</div></div>'
+      		].join('\n');
+		$(dom.newsWrapper).append(news);
+	}
+
+
+	var _setDom = function() { 
+		dom.newsWrapper = document.getElementById('news');
+	}
+
+	this.getRSS = function()  { 
+		$.ajax({ 
+			url: 'server/',
+			type: 'POST',
+			data: { getRSS : true },
+			success: function(data) {
+				_parse(data);
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+
+			}
+		});
 
 	}
-});
+	// constructor
+	_setDom();
+}
+
+
+
+var RSS = new RSSreader;
+RSS.getRSS();
